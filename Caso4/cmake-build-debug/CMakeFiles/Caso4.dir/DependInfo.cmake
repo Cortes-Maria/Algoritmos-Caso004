30964fa56bb6734569599386e9789cb3b8520537
Ixel Castro <ixcastro@ic-itcr.ac.cr>
2020-05-13 21:12:46 -0600

    Recuperando caso

--- Caso4/cmake-build-debug/CMakeFiles/Caso4.dir/DependInfo.cmake
+++ Caso4/cmake-build-debug/CMakeFiles/Caso4.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/cygdrive/c/Users/gollo/OneDrive - Estudiantes ITCR/Universidad/2020 Semestre I/Analisis de algoritmos/Caso4/main.cpp" "/cygdrive/c/Users/gollo/OneDrive - Estudiantes ITCR/Universidad/2020 Semestre I/Analisis de algoritmos/Caso4/cmake-build-debug/CMakeFiles/Caso4.dir/main.cpp.o"
+  "/home/ixel/Documents/Case004/Algoritmos-Caso004/Caso4/main.cpp" "/home/ixel/Documents/Case004/Algoritmos-Caso004/Caso4/cmake-build-debug/CMakeFiles/Caso4.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- Caso4/cmake-build-debug/CMakeFiles/Caso4.dir/cmake_clean.cmake
+++ Caso4/cmake-build-debug/CMakeFiles/Caso4.dir/cmake_clean.cmake
@@ -1,8 +1,7 @@
 file(REMOVE_RECURSE
   "CMakeFiles/Caso4.dir/main.cpp.o"
-  "Caso4.exe"
+  "Caso4"
   "Caso4.pdb"
-  "libCaso4.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
