24fd18897fe0a618eb2d972e9a4044306fffa332
María José Cortés <maria2000jose.mj@gmail.com>
2020-05-13 20:55:54 -0600

    sacar-sustantivos
    se implmentó la clase encargada de leer el documento de texto y sacar únicamente los sustantivos

---
+++ Caso4/cmake-build-debug/CMakeFiles/Caso4.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Caso4.dir/main.cpp.o"
+  "Caso4.exe"
+  "Caso4.pdb"
+  "libCaso4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Caso4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
